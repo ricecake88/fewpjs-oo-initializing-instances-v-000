5fd316887c473fa4caf2ba90569464f5317aa935
Grace Shih <vocalise@gmail.com>
2020-02-21 06:22:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 // Write your code here
-class Dog {
-  constructor(name, breed) {
-    this.name = name;
-    this.breed = breed;
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this._dessert = dessert;
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
   }
 }
